e4dff0303abfa27859b6e70e82355587ccffb86a
Yasir-401 <yassser.mohammmed122@gmail.com>
2023-11-28 21:08:57 +0100

    Integrated fragment_profile.xml with ProfileFragment.kt

--- MobileDevelopmentProject/app/src/main/java/edu/ap/mobiledevelopmentproject/ui/profile/ProfileFragment.kt
+++ MobileDevelopmentProject/app/src/main/java/edu/ap/mobiledevelopmentproject/ui/profile/ProfileFragment.kt
@@ -16,12 +16,38 @@ import edu.ap.mobiledevelopmentproject.ui.matches.MatchesFragment
 
 class ProfileFragment : Fragment() {
 
+    private lateinit var binding: FragmentProfileBinding
+
+    private val name = "John Smith"
+    private val description = "Plays in Mortsel, Belgium"
+    private val matches = 65
+    private val followers = 351
+    private val following = 90
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_profile, container, false)
-        return view
+    ): View {
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initializeUI()
+    }
+
+    private fun initializeUI() {
+        binding.name.text = name
+        binding.description.text = description
+
+        // Todo: stats
+//        val statsText = "Matches: $matches | Followers: $followers | Following: $following"
+//        binding.statsTextView.text = statsText
+
+        // Set up listeners or additional UI elements as needed
+
     }
 
 }
