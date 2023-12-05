424d1fd359f49397c830d8b249079c1dbf7063bd
Yasir-401 <yassser.mohammmed122@gmail.com>
2023-12-05 11:19:53 +0100

    Added saved changes to Firebase

--- MobileDevelopmentProject/app/src/main/java/edu/ap/mobiledevelopmentproject/ui/profile/ProfileFragment.kt
+++ MobileDevelopmentProject/app/src/main/java/edu/ap/mobiledevelopmentproject/ui/profile/ProfileFragment.kt
@@ -1,6 +1,5 @@
-package edu.ap.mobiledevelopmentproject.ui.profile
-
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,21 +7,20 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentTransaction
-import androidx.lifecycle.ViewModelProvider
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import edu.ap.mobiledevelopmentproject.R
 import edu.ap.mobiledevelopmentproject.databinding.FragmentProfileBinding
-import edu.ap.mobiledevelopmentproject.ui.matches.MatchesFragment
 
 class ProfileFragment : Fragment() {
 
     private lateinit var binding: FragmentProfileBinding
+    private lateinit var editName: EditText
+    private lateinit var editDescription: EditText
+    private lateinit var saveChangesButton: Button
+    private lateinit var cancelChangesButton: Button
 
-    private val name = "John Smith"
-    private val description = "Plays in Mortsel, Belgium"
-    private val matches = 65
-    private val followers = 351
-    private val following = 90
+    private lateinit var userId: String // Replace this with the actual user ID
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -39,15 +37,72 @@ class ProfileFragment : Fragment() {
     }
 
     private fun initializeUI() {
-        binding.name.text = name
-        binding.description.text = description
+        // Initialize UI elements
+        editName = view?.findViewById(R.id.editName)!!
+        editDescription = view?.findViewById(R.id.editDescription)!!
+        saveChangesButton = view?.findViewById(R.id.saveChangesButton)!!
+        cancelChangesButton = view?.findViewById(R.id.cancelChangesButton)!!
 
-        // Todo: stats
-//        val statsText = "Matches: $matches | Followers: $followers | Following: $following"
-//        binding.statsTextView.text = statsText
+        // Handle Edit Profile Button Click
+        view?.findViewById<Button>(R.id.editProfileButton)?.setOnClickListener {
+            // Show edit fields and buttons
+            showEditFields(true)
 
-        // Set up listeners or additional UI elements as needed
+            // Populate edit fields with current data
+            val currentName = view?.findViewById<TextView>(R.id.name)?.text.toString()
+            val currentDescription = view?.findViewById<TextView>(R.id.description)?.text.toString()
 
+            editName.setText(currentName)
+            editDescription.setText(currentDescription)
+        }
+
+        // Handle Save Changes Button Click
+        saveChangesButton.setOnClickListener {
+            // Save changes to Firebase database
+            val newName = editName.text.toString()
+            val newDescription = editDescription.text.toString()
+
+            // Update the UI with new data
+            view?.findViewById<TextView>(R.id.name)?.text = newName
+            view?.findViewById<TextView>(R.id.description)?.text = newDescription
+
+            // Hide edit fields and buttons
+            showEditFields(false)
+
+            // Save changes to Firebase
+            saveChangesToFirebase(userId, newName, newDescription)
+        }
+
+        // Handle Cancel Changes Button Click
+        cancelChangesButton.setOnClickListener {
+            // Hide edit fields and buttons
+            showEditFields(false)
+        }
     }
 
+    private fun showEditFields(show: Boolean) {
+        val visibility = if (show) View.VISIBLE else View.GONE
+        editName.visibility = visibility
+        editDescription.visibility = visibility
+        saveChangesButton.visibility = visibility
+        cancelChangesButton.visibility = visibility
+    }
+
+    private fun saveChangesToFirebase(userId: String, newName: String, newDescription: String) {
+        try {
+            val database: FirebaseDatabase = FirebaseDatabase.getInstance()
+            val usersRef: DatabaseReference = database.getReference("users")
+
+            // Replace "userId" with the actual user ID
+            val userRef: DatabaseReference = usersRef.child(userId)
+
+            // Update user data
+            userRef.child("name").setValue(newName)
+            userRef.child("description").setValue(newDescription)
+
+            Log.d("ProfileFragment", "Changes saved to Firebase")
+        } catch (e: Exception) {
+            Log.e("ProfileFragment", "Error saving changes to Firebase: ${e.message}")
+        }
+    }
 }
